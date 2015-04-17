2acab350358a1daa906b0ab6b28d313f90f57b10
Hoseok Lee <ho93suk@gmail.com>
2015-04-17 12:48:37 -0600

    Update Check.java

--- src/check/Check.java
+++ src/check/Check.java
@@ -15,16 +15,19 @@ import  java.util.concurrent.TimeUnit;
  *
  * @author jenniferschofield
  */
+// Check Class: Check the book if it is avilable
 public class Check {
-    int cardholder;
-    int bookid;
-    LocalDate duedate;
-
+    int cardholder; // Who has the book right now
+    int bookid; // Book ID
+    LocalDate duedate; // Due date of checked out book
+    
+    // Constructor
     public Check(int cardholder, int bookid,int days, LocalDate duedate) {
         this.cardholder = cardholder;
         this.bookid = bookid;
         this.duedate = duedate.plusDays(days);
     }
+    // Get info from data base
     public Check (String s){
         String [] ch= s.split(",=");
         cardholder =Integer.parseInt(ch[1]);
@@ -38,18 +41,23 @@ public class Check {
         return "C" + ",=" + cardholder + ",=" + bookid + 
                 ",=" + duedate.getYear()+",="+duedate.getMonth()+",=" +duedate.getDayOfMonth();
     }
+    // Check the book if it is check out
     public boolean ischeckedout(int item){
         return item==bookid;
     }
+    // Get patron's info
     public int getpatroncard (){
         return cardholder;
     }
+    // Check if the patron is in our data base
     public boolean ispatron(int c){
         return c==cardholder;
     }
+    // Get book ID
     public int getbookid (){
         return bookid;
     }
+    // Check duedate if it is over
        public boolean overdue(LocalDate now){
         return now.isAfter(duedate);
         
